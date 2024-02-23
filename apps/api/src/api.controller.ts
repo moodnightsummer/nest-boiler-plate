ad82dfc7f1c123946acf2a660979c5102d0d0ecb
jelly <moodnightsummer@naver.com>
2024-02-23 18:39:40 +0900

    feat : api에서 타 프로젝트 의존성 주입 및 typeorm 추가

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -1,12 +1,21 @@
+import { EntityService } from './../../../libs/entity/src/entity.service';
 import { Controller, Get } from '@nestjs/common';
 import { ApiService } from './api.service';
 
 @Controller()
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(
+    private readonly apiService: ApiService,
+    private readonly entityService: EntityService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.apiService.getHello();
+  }
+
+  @Get('/entity')
+  getEntity(): string {
+    return this.entityService.getHello();
   }
 }
