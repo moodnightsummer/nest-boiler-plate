6bf8c6ac87afa5873ece894dcdd979bac196668d
jelly <moodnightsummer@naver.com>
2024-02-25 19:22:22 +0900

    chore : eslint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  env: {
+    browser: true,
+    es2021: true,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-typescript/base',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  parser: ['@typescript-eslint/parser', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error'],
+    'no-param-reassign': ['error', { props: false }],
   },
 };
